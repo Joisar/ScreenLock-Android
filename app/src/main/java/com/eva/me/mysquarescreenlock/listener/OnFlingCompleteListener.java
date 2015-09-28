3305f0a3e7e32d55bf2ac5a91bd221c30f50c4d3
Radamés Ajna <radamajna@gmail.com>
2015-09-28 22:30:03 +0100

    Android Studio Version and English Translations for app messages

---
+++ app/src/main/java/com/eva/me/mysquarescreenlock/listener/OnFlingCompleteListener.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.eva.me.mysquarescreenlock.listener;
+
+/**
+ * @author EVA
+ * call when on fling direction is caught
+ */
+public interface OnFlingCompleteListener {
+	public void onFlingComplete(int curDirection) ;
+}
